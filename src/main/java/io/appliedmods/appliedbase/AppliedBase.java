2b9563429576fff89a5951fd6fc61ee074573106
JJL772 <jeremy.lorelli.1337@gmail.com>
2018-08-02 21:04:05 -0700

    Add core files

---
+++ src/main/java/io/appliedmods/appliedbase/AppliedBase.java
@@ -0,0 +1,24 @@
+package io.appliedmods.appliedbase;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = "appliedbase", name = "Applied Base", version = "0.0.1")
+public class AppliedBase
+{
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent ev)
+    {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent ev)
+    {
+
+    }
+}
